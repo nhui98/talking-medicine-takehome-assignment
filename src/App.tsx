53537befe24ac932a21cc8fde9be1859255cd148
Nathan Hui <hui.n1998@gmail.com>
2022-10-04 20:21:33 +0100

    project setup - vite, react, eslint, prettier, husky, lintstaged, react-router-dom typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Home from "@pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
