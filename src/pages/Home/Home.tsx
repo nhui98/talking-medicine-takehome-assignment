4c06030f3d7aa18c23ff72557cc490bbd9595e9c
Nathan Hui <hui.n1998@gmail.com>
2022-10-05 21:53:47 +0100

    home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,46 @@
+import PostCard from "@components/home/PostCard/PostCard";
+import { useEffect, useState } from "react";
+import FetchReddit, { RedditApiResponse } from "src/api/fetchReddit";
+
+import s from "./Home.module.scss";
+
+const Home = () => {
+  const [redditData, setRedditData] = useState<RedditApiResponse | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const data = await FetchReddit();
+        if (!data) return;
+        setRedditData(data);
+      } catch (error) {
+        setError(true);
+        if (error instanceof Error) console.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  if (loading) return <h1>Loading</h1>;
+
+  if (error) return <h1>Error</h1>;
+
+  return (
+    <main className={s.home}>
+      <section className={s.container}>
+        {redditData?.children.map(({ data }) => (
+          <PostCard key={data.id} data={data} />
+        ))}
+      </section>
+    </main>
+  );
+};
+
+export default Home;
